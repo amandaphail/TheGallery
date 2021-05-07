ec86e7d5ec70accbc9ff8410adf4711197e9d28d
Amanda <amandalphail@gmail.com>
2021-05-07 17:49:40 -0400

    fix migration

--- db/migrate/20210507213344_create_gallery_images.rb
+++ db/migrate/20210507213344_create_gallery_images.rb
@@ -2,7 +2,7 @@ class CreateGalleryImages < ActiveRecord::Migration[6.1]
   def change
     create_table :gallery_images do |t|
       t.string :frame_color
-      t.int :position
+      t.integer :position
       t.references :gallery, null: false, foreign_key: true
       t.references :image, null: false, foreign_key: true
 
