d8d5abc2694e4369611889a4e9687371f352497a
Amanda <amandalphail@gmail.com>
2021-05-08 12:43:09 -0400

    images controller updated

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -10,42 +10,44 @@ class ImagesController < ApplicationController
 
 
   def index
-    @images = Image.all
+    @images = Gallery.find(params[:gallery_id]).images 
 
     render json: @images
   end
 
   def show
-    @image = Image.find(params[:id])
-  
+    # @image = Image.find(params[:id])
+
     render json: @image 
   end
 
   def create
-    @image = Image.new(image_params)
+    @gallery = Gallery.find(params[:gallery_id])
+    @image = @gallery.images.build(image_params)
 
     if @image.save
-      render json: @image
+      render json: @image, status :created
     else
-      render json: @image.errors
+      render json: @image.errors, status :unprocessable_entity
     end
   end
 
   def update
-    @image = Image.find(params[:id])
+    # @image = Image.find(params[:id])
 
     if @image.update(image_params)
       render json: @image
     else
-      render json: @image.errors 
+      render json: @image.errors, status :unprocessable_entity
     end
   end
 
   def destroy
-    @image = Image.find(params[:id])
+    # @image = Image.find(params[:id])
 
-    @image.destroy
-    render json: {message: "Image has been deleted"}
+    if @image.destroy
+      render json: {message: "Image has been deleted"}
+    end
   end
 
   def image_params
