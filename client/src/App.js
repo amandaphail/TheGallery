31b3809e8ad8ecb9de47bcee0fbd884496944dce
Amanda <amandalphail@gmail.com>
2021-05-08 18:21:15 -0400

    title page renders in browser

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import {Route} from "react-router-dom"
+import Title from "./screens/Title.jsx"
 
 function App() {
   return (
     <div className="App">
-      
+      <Route>
+        <Title />
+      </Route>
     </div>
   );
 }
