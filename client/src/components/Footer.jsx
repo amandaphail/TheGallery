4640cfd77e21d335eba75ba59e098ea612105f9d
Amanda <amandalphail@gmail.com>
2021-05-08 18:56:01 -0400

    footer styles

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import "./CSS/footer.css"
+import image from "../images/GitHub-Mark-64px.png"
+import {Link} from "react-router-dom"
 
 export default function Footer() {
   return (
-    <div>
-      Footer
+    <div id="footer">
+      <Link to="https://github.com/amandaphail/TheGallery">
+        {/* why not working? */}
+      <img alt="Github Link" src={image} />
+      </Link>
     </div>
   )
 }
