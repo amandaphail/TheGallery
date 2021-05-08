c9139d07c865c6e7dc07529e3fcce9a4cee7b002
Amanda <amandalphail@gmail.com>
2021-05-08 18:27:44 -0400

    basic react routes set up to display in browser

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
 import {Route} from "react-router-dom"
 import Title from "./screens/Title.jsx"
+import GettingStarted from "./screens/GettingStarted.jsx"
+import GalleryDisplay from "./screens/GalleryDisplay.jsx"
+import Nav from "./components/Nav.jsx"
+import Footer from "./components/Footer.jsx"
 
 function App() {
   return (
     <div className="App">
+      <Nav/>
       <Route>
         <Title />
       </Route>
+      <Route>
+        <GettingStarted />
+      </Route>
+      <Route>
+        <GalleryDisplay />
+      </Route>
+      <Footer />
     </div>
   );
 }
---
+++ client/src/screens/GalleryDisplay.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function GalleryDisplay() {
+  return (
+    <div>
+      Gallery Display
+    </div>
+  )
+}
