0e531e13e6ae96bc2be27bbe3d564a194f84323c
Amanda <amandalphail@gmail.com>
2021-05-08 13:15:31 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,12 @@ Rails.application.routes.draw do
   # get 'galleries/delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :images
-  resources :galleries
+  resources :gallery_images
+
+  resources :galleries do
+    resources :images
+  end
+
+  get "/images", to: "images#all_images"
 
 end
