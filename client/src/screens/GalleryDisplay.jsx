dc4f3c429d7f849c185f7271a44b7bf5bc61ec53
Amanda <amandalphail@gmail.com>
2021-05-08 22:18:07 -0400

    gallery display content and stylings

--- client/src/screens/GalleryDisplay.jsx
+++ client/src/screens/GalleryDisplay.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import "./CSS/gallerydisplay.css"
 
 export default function GalleryDisplay() {
   return (
-    <div>
+    <div id="gallerydisplay">
       Gallery Display
+       
+      <div id="contentcontainer">
+        <div>
+          look
+          {/* <GalleryContent /> */}
+        </div>
+        <div>
+          {/* desk image */}
+        </div>
+      </div>
+
+      <form>
+        <div id="displayform">
+        <label>Image 1: </label>
+        <input className="displayinput" type="text"/>
+
+        <label>Image 2: </label>
+        <input className="displayinput" type="text"/>
+
+        <label>Image 3: </label>
+        <input className="displayinput" type="text"/>
+
+        <label>Image 4: </label>
+        <input className="displayinput" type="text"/>
+        </div>
+      </form>
     </div>
   )
 }
