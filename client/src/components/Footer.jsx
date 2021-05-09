cb2ecb8148f7b462b1423ddc135aca2e3c695ee7
Amanda <amandalphail@gmail.com>
2021-05-08 23:11:16 -0400

    nav and footer github links working

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,14 +1,12 @@
 import "./CSS/footer.css"
 import image from "../images/GitHub-Mark-64px.png"
-import {Link} from "react-router-dom"
 
 export default function Footer() {
   return (
     <div id="footer">
-      <Link to="https://github.com/amandaphail/TheGallery">
-        {/* why not working? */}
-      <img alt="Github Link" src={image} />
-      </Link>
+      <a href="https://github.com/amandaphail/TheGallery">
+        <img alt="Github Link" src={image} />
+      </a>
     </div>
   )
 }
--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,6 +1,7 @@
 import {Link} from "react-router-dom"
 import "./CSS/nav.css"
 
+
 export default function Nav() {
   return (
     <div id="nav">
@@ -9,7 +10,9 @@ export default function Nav() {
       <Link to="">Home</Link>
       <Link to="">Getting Started</Link>
       <Link to="">Gallery View</Link>
-      <Link to="">Github</Link>
+      <a href="https://github.com/amandaphail/TheGallery">
+        GitHub
+      </a>
       </div>
     </div>
   )
