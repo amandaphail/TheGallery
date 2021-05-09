b163664286f2e8933996e515fc0413dddc8d9dbb
Amanda <amandalphail@gmail.com>
2021-05-08 22:59:27 -0400

    nav styling and content

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import {Link} from "react-router-dom"
+import "./CSS/nav.css"
 
 export default function Nav() {
   return (
-    <div>
-      Nav
+    <div id="nav">
+      <div id="links">
+
+      <Link to="">Home</Link>
+      <Link to="">Getting Started</Link>
+      <Link to="">Gallery View</Link>
+      <Link to="">Github</Link>
+      </div>
     </div>
   )
 }
