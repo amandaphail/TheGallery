000a8a004a230b2c122156432a93db9ab6e56a78
Amanda <amandalphail@gmail.com>
2021-05-08 21:36:47 -0400

    getting started content and styling

--- client/src/screens/GettingStarted.jsx
+++ client/src/screens/GettingStarted.jsx
@@ -4,8 +4,29 @@ export default function GettingStarted() {
   return (
     <div id="gettingstarted">
       <h2>
-      Getting Started
+        Getting Started
       </h2>
+      
+      <p>Let's talk about the space</p>
+
+      <form id="gsform">
+        <div>
+        <label>Number of frames: </label>
+        <select>
+          <option>4</option>
+          <option>5</option>
+          <option>6</option>
+          <option>7</option>
+          <option>8</option>
+        </select>
+        </div>
+
+        <div>
+        <label>Wall Color: </label>
+        <input type="color" />
+        </div>
+        
+      </form>
     </div>
   )
 }
