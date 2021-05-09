e9ab6980837735e314be82abbd412b5283b21bdb
Amanda <amandalphail@gmail.com>
2021-05-08 22:44:59 -0400

    styling adjustments

--- client/src/screens/GalleryDisplay.jsx
+++ client/src/screens/GalleryDisplay.jsx
@@ -3,11 +3,10 @@ import "./CSS/gallerydisplay.css"
 export default function GalleryDisplay() {
   return (
     <div id="gallerydisplay">
-      Gallery Display
+      
        
       <div id="contentcontainer">
         <div>
-          look
           {/* <GalleryContent /> */}
         </div>
         <div>
@@ -17,16 +16,16 @@ export default function GalleryDisplay() {
 
       <form>
         <div id="displayform">
-        <label>Image 1: </label>
+        <label>1: </label>
         <input className="displayinput" type="text"/>
 
-        <label>Image 2: </label>
+        <label> 2: </label>
         <input className="displayinput" type="text"/>
 
-        <label>Image 3: </label>
+        <label> 3: </label>
         <input className="displayinput" type="text"/>
 
-        <label>Image 4: </label>
+        <label> 4: </label>
         <input className="displayinput" type="text"/>
         </div>
       </form>
