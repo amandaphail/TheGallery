3b999cfc3353f0d75fe8385e0daf34e5627265b0
Amanda <amandalphail@gmail.com>
2021-05-10 19:24:45 -0400

    useState and handle change working

--- client/src/components/GalleryForm.jsx
+++ client/src/components/GalleryForm.jsx
@@ -1,12 +1,31 @@
 import "./CSS/galleryform.css"
+import {useState} from "react"
 
 export default function GalleryForm() {
+
+  const [gallery, setGallery] = useState({
+    number_of_frames: 4,
+    wall_color: ""
+  })
+
+
+  function handleChange(event) {
+    let { id, value } = event.target
+    setGallery((prevState) => ({
+      ...prevState, [id]: value
+    }))
+  }
+
+  function handleSubmit() {
+    
+  }
+
   return (
     <div>
       <form id="gsform">
         <div className="formdivs">
         <label>Number of frames: </label>
-        <select>
+        <select id="number_of_frames" form="gsform" value = {gallery.number_of_frames} onChange={handleChange}>
           <option>4</option>
           <option>5</option>
           <option>6</option>
@@ -17,7 +36,7 @@ export default function GalleryForm() {
 
         <div className="formdivs">
         <label>Wall Color: </label>
-        <input type="color" />
+          <input type="color" id="wall_color" value={gallery.wall_color} onChange={handleChange}/>
         </div>
 
         <div className="formdivs">
