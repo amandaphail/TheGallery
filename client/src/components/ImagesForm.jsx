65fda239b19c89000e3bb76522d6152be15ba351
Amanda <amandalphail@gmail.com>
2021-05-10 22:17:56 -0400

    figure out image form submissions

--- client/src/components/ImagesForm.jsx
+++ client/src/components/ImagesForm.jsx
@@ -1,4 +1,6 @@
 import "./CSS/imagesform.css"
+import { useState } from "react"
+import {postImage} from "../services/images.jsx"
 
 export default function ImagesForm(props) {
 
@@ -6,22 +8,47 @@ export default function ImagesForm(props) {
 
   console.log(`Image Form: ${frameNumber}`)
 // if gallery frame number set - this how many image inputs to have & which gallery template to use
+  
+  const [image, setImage] = useState([])
+  // {
+  //   url: ""
+  // }
+  
+  function handleChange(event) {
+    let { id, value } = event.target
+    setImage((prevState) => ({
+      ...prevState, [id]: value
+    }))
+
+  }
+
+  console.log(image)
+  
+  async function handleSubmit(event) {
+    event.preventDefault()
+    await postImage(image)
+    //loop here?
+    //image.map
+  }
+
+
 
   return (
     <div>
-       <form>
+       <form onSubmit={handleSubmit}>
         <div id="displayform">
         <label>1: </label>
-        <input className="displayinput" type="text"/>
+          <input id="1" className="displayinput" type="text" onChange={handleChange} />
+          {/* id = i --> once loop over */}
 
         <label> 2: </label>
-        <input className="displayinput" type="text"/>
+        <input id ="2" className="displayinput" type="text" onChange={handleChange}/>
 
         <label> 3: </label>
-        <input className="displayinput" type="text"/>
+        <input id="3" className="displayinput" type="text" onChange={handleChange}/>
 
         <label> 4: </label>
-        <input className="displayinput" type="text"/>
+        <input id="4" className="displayinput" type="text" onChange={handleChange}/>
         
         <input className="displayinput" type="submit" />
         </div>
