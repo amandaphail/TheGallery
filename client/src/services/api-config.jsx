8be9b92aae1b1bc1774dea1f9e00e7b90c6f43ef
Amanda <amandalphail@gmail.com>
2021-05-13 16:12:04 -0400

    heroku set up

--- client/src/services/api-config.jsx
+++ client/src/services/api-config.jsx
@@ -1,7 +1,17 @@
 import axios from 'axios';
+let apiUrl
+let apiUrls = {production: "https://the-gallery-mandii.herokuapp.com/", developent: "http://localhost:3000"}
+
+if (window.location.hostname === "localhost") {
+  apiUrl = apiUrls.development;
+} else {
+  apiUrl = apiUrls.production;
+}
 
 const api = axios.create({
-  baseURL: "http://localhost:3000"
+  baseURL: apiUrl
+  
 })
+
 
 export default api;
