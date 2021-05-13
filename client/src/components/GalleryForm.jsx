65b1194ba6fb7439129548ac28bdfe65f19c0b47
Amanda <amandalphail@gmail.com>
2021-05-13 11:42:43 -0400

    figure out form display logic

--- client/src/components/GalleryForm.jsx
+++ client/src/components/GalleryForm.jsx
@@ -1,6 +1,6 @@
 import "./CSS/galleryform.css"
 import {useState} from "react"
-import { createGallery, updateGallery } from "../services/galleries"
+import { createGallery, updateGallery, getGallery } from "../services/galleries"
 
 export default function GalleryForm(props) {
 
@@ -30,12 +30,54 @@ export default function GalleryForm(props) {
     
   }
 
+  async function handleUpdate(event) {
+    event.preventDefault()
+    await updateGallery(galleryID, gallery)
+    // setGalleryID(yourGallery.id)
+    // history push to gallery display page?
+    
+  }
+
+
+
   //if !galleryID - then {create()} - this will have create form which calls handle submit
   //if galleryID - then {update()} - this will have update form which calls handle update - also have it populate with what the gallery already is
 
+  // console.log(galleryID)
+  const formDisplay = () => {
+    
+    if (!galleryID) {
+      return createForm() 
+    } else {
+      console.log("You've already created a gallery!")
+      // getYourGallery()
+      return updateForm()
+      // infinite loop happening in whole else statement
+    }
+  }
+    
+//     if (galleryID) {
+//       console.log("You've already created a gallery!")
+//     getYourGallery()
+//     return updateForm() 
+//   } else {
+//     return createForm()
+//   }
+// }
+
+  const getYourGallery = async () => {
+    const info = await getGallery(galleryID);
+    setGallery(info);
+    // this is allowing it to populate with what you have already in your gallery?
+    console.log(gallery)
+  };
 
   const createForm = () => {
     return (
+      <div>
+
+      <h2>Create</h2>
+
       <form id="gsform" onSubmit={handleSubmit}>
         <div className="formdivs">
         <label>Number of frames: </label>
@@ -51,16 +93,44 @@ export default function GalleryForm(props) {
         </div>
 
         <div className="formdivs">
-          <input type="submit" />
+          <input type="submit"/>
           {/* {button()} */}
         </div>
         
       </form>
+      </div>
     )
   }
 
 
+  const updateForm = () => {
+    return (
+      <div>
+        <h2>Update</h2>
+      <form id="gsform" onSubmit={handleUpdate}>
+        <div className="formdivs">
+        <label>Number of frames: </label>
+        <select id="number_of_frames" form="gsform" value = {gallery.number_of_frames} onChange={handleChange}>
+          <option>4</option>
+          <option>5</option>
+        </select>
+        </div>
 
+        <div className="formdivs">
+        <label>Wall Color: </label>
+          <input type="color" id="wall_color" value={gallery.wall_color} onChange={handleChange}/>
+        </div>
+
+        <div className="formdivs">
+        <input type="submit"/>
+          {/* {button()} */}
+        </div>
+        
+        </form>
+        {/* delete button */}
+      </div>
+    )
+  }
 
 
 // with conditional in handle submit function
@@ -80,27 +150,32 @@ export default function GalleryForm(props) {
   
   // console.log(galleryID)
   
-  const button = () => {
-    // if (galleryID) {
-    //   return (<button onclick={update()}>Update</button>)
-    // } else {
-    //   return (
+  // const button = () => {
+  //   // if (galleryID) {
+  //   //   return (<button onclick={update()}>Update</button>)
+  //   // } else {
+  //   //   return (
     
-      return  <input type="submit"/>
-        // )
+  //     return  <input type="submit"/>
+  //       // )
     
-  }
+  // }
 
-  const update = () => {
-    console.log(galleryID)
-    updateGallery(galleryID, gallery)
-    console.log("update button works")
-    console.log(galleryID)
-  }
+  // const update = () => {
+  //   console.log(galleryID)
+  //   updateGallery(galleryID, gallery)
+  //   console.log("update button works")
+  //   console.log(galleryID)
+  // }
 
   return (
     <div>
-      {createForm()}
+      {formDisplay()}
+      {/* {createForm()} */}
+      {/* {updateForm()} */}
+      
+
+
       {/* <form id="gsform" onSubmit={handleSubmit}>
         <div className="formdivs">
         <label>Number of frames: </label>
