692eb01bda5327b8204f971d11f4181a4cc431bf
Amanda <amandalphail@gmail.com>
2021-05-14 02:10:08 -0400

    set up

--- client/src/services/api-config.jsx
+++ client/src/services/api-config.jsx
@@ -1,22 +1,22 @@
 import axios from 'axios';
-// let apiUrl
-// let apiUrls = {production: "https://the-gallery-mandii.herokuapp.com/", developent: "http://localhost:3000"}
+let apiUrl
+let apiUrls = {production: "https://the-gallery-mandii.herokuapp.com/", developent: "http://localhost:3000"}
 
-// if (process.env.NODE_ENV === 'production') {
-//   apiUrl = apiUrls.production;
-// } else {
-//   apiUrl = apiUrls.development;
-// }
-
-// const api = axios.create({
-//   baseURL: apiUrl
-  
-// })
+if (process.env.NODE_ENV === 'production') {
+  apiUrl = apiUrls.production;
+} else {
+  apiUrl = apiUrls.development;
+}
 
 const api = axios.create({
-  baseURL: "http://localhost:3000"
+  baseURL: apiUrl
   
 })
+
+// const api = axios.create({
+//   baseURL: "http://localhost:3000"
+  
+// })
 
 
 export default api;
