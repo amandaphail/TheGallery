536fc751ed1b86f8a4c520a78ef8d146b3182623
Amanda <amandalphail@gmail.com>
2021-05-13 22:54:01 -0400

    getting started styling

--- client/src/screens/GettingStarted.jsx
+++ client/src/screens/GettingStarted.jsx
@@ -5,12 +5,12 @@ export default function GettingStarted(props) {
 
   return (
     <div id="gettingstarted">
-      <h1>
+      <h1 id="h1">
         Getting Started
       </h1>
       
       <div id ="formcontainer">
-      <h4>Let's talk about the space</h4>
+      <h2 id="h2">Let's talk about the space</h2>
         <GalleryForm setToggle={props.setToggle} setGalleryID={props.setGalleryID} galleryID={props.galleryID}/>
       </div>
     </div>
